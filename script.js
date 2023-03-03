514e6dad9b6a5b07dae8c5cf3126fbe6ca436687
O-Gerler <azurmendi5o@outlook.com>
2023-03-03 21:09:13 +0100

    primera version

---
+++ script.js
@@ -0,0 +1,35 @@
+const createDelBtn = () => {
+    const delBtn = document.createElement('button')
+    delBtn.classList.add('boton-delete')
+    delBtn.setAttribute('type', 'button')
+    delBtn.innerHTML = "-"
+
+    delBtn.addEventListener('click', e => {
+        const newItem = delBtn.parentElement
+        list.removeChild(newItem)
+    })
+
+    return delBtn
+}
+
+const todosName = document.querySelector('input')
+const addBtn = document.getElementById('add-btn')
+const list = document.querySelector('ul')
+
+addBtn.addEventListener('click', e => {
+
+    if(todosName.value.trim() !== ""){
+        let text = todosName.value
+
+        const newItem = document.createElement('li')
+
+        newItem.innerText = text
+
+        const delBtn = createDelBtn()
+
+        newItem.appendChild(delBtn)
+        list.appendChild(newItem)
+
+        todosName.value = ""
+    }
+})
